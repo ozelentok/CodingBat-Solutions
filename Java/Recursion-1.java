860b5dc22d5b870d3138399fbf0d28ecf39fa4f5
MajesticOl <36264959+MajesticOl@users.noreply.github.com>
2018-02-11 21:20:09 +0100

    Possibly a smoother version of countHi2 example
    Maybe its a bit smoother that way. Regards - oliver

--- Java/Recursion-1.java
+++ Java/Recursion-1.java
@@ -238,23 +238,18 @@ public String stringClean(String str)
 }
 
 // Given a string, compute recursively the number of times lowercase "hi" appears in the string, however do not count "hi" that have an 'x' immedately before them.
-public int countHi2(String str)
-{
-	if(str.length() < 2)
-		return 0;
-	if(str.length() == 2)
-		return (str.equals("hi")) ? 1 : 0;
-	if(str.charAt(0) == 'x')
-	{
-		if(str.substring(1, 3).equals("hi"))
-			return countHi2(str.substring(3));
-		return countHi2(str.substring(1));
-	}
-	if(str.substring(0, 2).equals("hi"))
-		return 1 + countHi2(str.substring(2));
-	if(str.substring(1, 3).equals("hi"))
-		return 1 + countHi2(str.substring(3));
-	return countHi2(str.substring(2));
+public int countHi2(String str) {
+  if(str.length()<=2){
+   return (str.equals("hi"))? 1:0;
+  }
+  
+  if (str.charAt(0)=='x'&&str.substring(1,3).equals("hi")){
+    return countHi2(str.substring(3));
+  }
+  if(str.substring(0,2).equals("hi")){
+    return 1+countHi2(str.substring(2));
+  }
+  return countHi2(str.substring(1));
 }
 
 // the Above Problem can also be done using string tools provided by java API
